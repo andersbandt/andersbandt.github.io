be9feb7f4d952ebf27afe70d4af348e69651a2e9
andersbandt <andersbandt@yahoo.com>
2026-02-06 04:00:17 +0000

    Getting things back in sync after SF work

--- ps1obsidianconvert.ps1
+++
@@ -1,9 +0,0 @@
-# Set your Python executable path (adjust if needed)
-$pythonPath = "C:\Users\ander\AppData\Local\Programs\Python\Python312\python.exe"
-
-# Set the full path to your script
-$scriptPath = "C:\Users\ander\OneDrive\Code\python\text_processing\md_to_html.py"
-
-# Run the script
-& $pythonPath $scriptPath
-
